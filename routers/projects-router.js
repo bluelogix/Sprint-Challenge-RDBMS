1e40b814bb4ae5ef6d9d79bbe4835ae2ca62d537
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-01 12:39:56 -0500

    post for projects completed

---
+++ routers/projects-router.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const knex = require('knex'); 
+
+const knexConfig = require('../knexfile.js')
+const db = knex(knexConfig.development) 
+
+
+  
+  // GET
+  router.get('/', (req, res) => {
+    db('projects')
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+  });
+
+//   needed to post: name, description, completed
+  router.post('/', (req, res) => {
+    db('projects')
+    .insert(req.body)
+    .then(projectId=> {
+      const [id] = projectId
+      db('projects')
+      .where({ id })
+      .first()
+      .then(project => {
+        res.status(201).json(project)
+      })
+    })
+    .catch(err => {
+      res.status(500).json({err: 'Error adding new project!' })
+    })
+  })
+
+
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const projectsRouter = require('./routers/projects-router.js');
+// const actionsRouter = require('./routers/actions-router.js');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-
+server.use('/api/projects', projectsRouter);
+// server.use('/api/actions', actionsRouter);
 
 module.exports = server;
